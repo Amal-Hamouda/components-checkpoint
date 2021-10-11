dc8f5ad42057932180ef00057d9a992614d16887
Amal-Hamouda <amall.hammouda@gmail.com>
2021-10-11 10:34:14 +0100

    img

--- src/components/Profile/Adress.js
+++ src/components/Profile/Adress.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { createRef } from "react";
-import "./Address.css"
+import "./Adress.css"
 const Adress = () => <h1 className="Adress">Address: Ain Zaghouan</h1>;
 export default Adress;
