8f58534b147605d09ae13a80505737cdcf44e959
Amal-Hamouda <amall.hammouda@gmail.com>
2021-10-11 10:18:15 +0100

    project first commit

---
+++ src/components/Profile/Address.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRef } from "react";
+import "./Address.css"
+const Adress = () => <h1 className="Adress">Address: Ain Zaghouan</h1>;
+export default Adress;
---
+++ src/components/Profile/FullName.js
@@ -0,0 +1,4 @@
+import React from "react";
+import "./FullName.css"
+const Name = () => <h1 className="name">Full Name : Amal HAMMOUDA</h1>;
+export default Name;
